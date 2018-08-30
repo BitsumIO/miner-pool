df85339e091461d448f4941bccafa894f149c47f
chen <gakiclin@gmail.com>
2018-08-30 17:17:02 +0800

    update minerpool index

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-module.exports.F2MinerPool = require('./src/f2_miner_pool')
-module.exports.AntMinerPool = require('./src/ant_miner_pool')
+const F2MinerPool = require('./src/f2_miner_pool')
+const AntMinerPool = require('./src/ant_miner_pool')
+
+module.exports = function createMinerPoolFactory (type, ...args) {
+  switch (type) {
+    case config.MINER_POOL_TYPES.ANT_POOL: {
+      return AntMinerPool.create(...args)
+    }
+    case config.MINER_POOL_TYPES.F2_POOL: {
+      return F2MinerPool.create(...args)
+    }
+    default: {
+      throw new Error('Unknow Miner Pool Type: ', type)
+    }
+  }
+}
+
+module.exports.config = require('./src/config')
