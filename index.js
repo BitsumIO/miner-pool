0a05958e2846b8562fa1849a8c9fa59e86ecd30a
chen <gakiclin@gmail.com>
2018-11-15 11:29:42 +0800

    export mph

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const config = require('./src/config')
 const F2MinerPool = require('./src/f2_miner_pool')
 const AntMinerPool = require('./src/ant_miner_pool')
 const BWMinerPool = require('./src/bw_miner_pool')
+const MPHMinerPool = require('./src/mph_miner_pool')
 
 module.exports = function createMinerPoolFactory (type, ...args) {
   switch (type) {
@@ -13,6 +14,9 @@ module.exports = function createMinerPoolFactory (type, ...args) {
     }
     case config.MINER_POOL_TYPES.BW_POOL: {
       return BWMinerPool.create(...args)
+    }
+    case config.MINER_POOL_TYPES.MPH_POOL: {
+      return MPHMinerPool.create(...args)
     }
     default: {
       throw new Error('Unknow Miner Pool Type: ', type)
